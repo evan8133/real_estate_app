b56fe74567ef08afa3c94f205d13c91c4afae48c
halahakim119 <97341494+halahakim119@users.noreply.github.com>
2023-07-11 17:07:48 +0300

    updates

--- macos/Flutter/ephemeral/flutter_export_environment.sh
+++ macos/Flutter/ephemeral/flutter_export_environment.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=C:\scr\flutter"
-export "FLUTTER_APPLICATION_PATH=C:\Users\divan\Documents\GitHub\real_estate_app\real_estate_app"
+export "FLUTTER_ROOT=C:\src\flutter"
+export "FLUTTER_APPLICATION_PATH=C:\Users\msi-pc\OneDrive\Documents\GitHub\real_estate_app"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_BUILD_DIR=build"
 export "FLUTTER_BUILD_NAME=1.0.0"
